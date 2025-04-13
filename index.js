c7b742279296ccace4251f664135aa647327829c
root <root@ip-172-31-14-62.ap-southeast-2.compute.internal>
2025-04-13 21:55:28 +0000

    test

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const https = require('https');
+const http = require('http');
+const express = require('express');
+const app = express();
+
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/woostaa.com/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/woostaa.com/cert.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/woostaa.com/chain.pem')
+};
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
+// HTTP server for redirecting HTTP to HTTPS
+http.createServer((req, res) => {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+  res.end();
+}).listen(80); // Redirect HTTP traffic to HTTPS
+
+// HTTPS server
+https.createServer(options, app).listen(443, () => {
+  console.log('HTTPS server running on port 443');
+});
+
+
